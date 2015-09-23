57edf78eac17e91bab2799bae00f9e24fcc926ad
timerg <ggteven@gmail.com>
2015-09-23 15:46:25 +0800

    .

--- pwmADC/code/datamining.r
+++ pwmADC/code/datamining.r
@@ -1,6 +1,10 @@
 setwd("~/GitHub/hspice/pwmADC/code")
 input <- read.table("newcreation.txt", sep = "=", as.is = 4)
 yval = log10(as.numeric(input[, 4]))
+png("linearity_log")
 plot <- plot( input[, 2],yval, type = "b")
+dev.off
 x11()
+png("linearity")
 plot <- plot( input[, 2],input[, 4], type = "b")
+dev.off
